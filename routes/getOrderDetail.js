09d4da25575c1e694d9353f560e06abe6ee342da
Rodrigo Sonoki <rodrigosonoki@iMac-de-Rodrigo.local>
2020-07-18 20:02:30 -0300

    feat: 401 error for order.id different than req.id

--- routes/getOrderDetail.js
+++ routes/getOrderDetail.js
@@ -5,8 +5,11 @@ router.post("/", async (req, res) => {
   const orders = await Order.findOne({ pedidoId: req.body.pedidoId }).exec();
   if (!orders) {
     return res.status(400).json({ error: "You have no orders...yet!" });
+  } else if (req.user.id != orders.sellerId) {
+    return res.status(401).json({ error: "Esse pedido não é seu." });
+  } else {
+    res.json({ order: orders.products });
   }
-  res.json({ order: orders.products });
 });
 
 module.exports = router;
